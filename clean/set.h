dc00baa763b002c8e49bea37b6466911a9a47279
rtjk <47841774+rtjk@users.noreply.github.com>
2024-04-06 21:41:31 +0200

    delete commented code in api.h

--- clean/set.h
+++ clean/set.h
@@ -5,16 +5,6 @@
 #define CATEGORY___nist-level__ 1
 #define __target__ 1
 
-/* #define AES_CTR_CSPRNG
-#define SHA3_HASH */
-/* #define OMIT_SEED_TREE ""
-#define KECCAK_EXTERNAL_ENABLE */
-
-//  #define SHA_3_LIBKECCAK
-//  #define SPEED 1
-//  #define NO_TREES 1
-//  #define SIG_SIZE 1
-
 #if defined(SPEED)
     #define NO_TREES 1
 #endif

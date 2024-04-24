640c06f85e32928913db4e4330a42fcaddc4f8bf
rtjk <47841774+rtjk@users.noreply.github.com>
2024-04-24 14:28:54 +0200

    remove internal shake implementation in favor of PQClean/common

--- clean/keccakf1600.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-void __namespace__KeccakF1600_StateExtractBytes(uint64_t *state, unsigned char *data,
-                                   uint32_t offset, uint32_t length);
-void __namespace__KeccakF1600_StateXORBytes(uint64_t *state, const unsigned char *data,
-                               uint32_t offset, uint32_t length);
-void __namespace__KeccakF1600_StatePermute(uint64_t *state);

13d021defaff7e66b7b9dd06a3330f00adce4397
rtjk <47841774+rtjk@users.noreply.github.com>
2024-07-06 21:16:03 +0200

    generate

---
+++ generate/crypto_sign/cross-rsdp-192-balanced/avx2/architecture_detect.h
@@ -0,0 +1,15 @@
+
+
+
+#pragma once
+
+#include <immintrin.h>
+
+#include <pmmintrin.h>
+
+#include <stdalign.h>
+#include <wmmintrin.h>
+#include <x86intrin.h>
+
+#define EPI8_PER_REG 32
+#define EPI16_PER_REG 16
